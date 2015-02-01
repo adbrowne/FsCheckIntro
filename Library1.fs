8f46d3ca760397adada7fbfb2b6871c808b0c243
Andrew Browne <brownie@brownie.com.au>
2015-02-01 22:01:37 +1100

    Initial commit

---
+++ Library1.fs
@@ -0,0 +1,25 @@
+﻿namespace FsCheckIntro
+
+open FsCheck.Xunit
+
+module Tests = 
+
+    let rec quickSort = function
+    | [] -> []
+    | x::xs -> 
+        let lhs = xs |> List.filter (fun a -> a < x)
+        let rhs = xs |> List.filter (fun a -> a >= x)
+        List.append (quickSort lhs) (x::(quickSort rhs))
+
+    [<Property>]
+    let ``quickSort is idempotent`` (xs : int list) =
+        let sorted = quickSort xs
+        sorted = quickSort sorted
+
+    [<Property>]
+    let ``quickSort matches Enumerable.OrderBy`` (xs : int list) = 
+        let quickSorted = quickSort xs
+        let systemSorted = 
+            System.Linq.Enumerable.OrderBy(xs, fun x -> x) 
+            |> List.ofSeq
+        quickSorted = systemSorted
---
+++ Script.fsx
@@ -0,0 +1,28 @@
+﻿#r "./packages/FsCheck.1.0.4/lib/net45/FsCheck.dll"
+open FsCheck
+
+let rec quickSort = function
+| [] -> []
+| x::xs -> 
+    let lhs = xs |> List.filter (fun a -> a < x)
+    let rhs = xs |> List.filter (fun a -> a >= x)
+    List.append (quickSort lhs) (x::(quickSort rhs))
+
+let quickSortIsIdempotent (xs : int list) =
+    let sorted = quickSort xs
+    sorted = quickSort sorted
+
+let quickSortMatchesEnumerableOrderBy (xs : int list) = 
+    let quickSorted = quickSort xs
+    let systemSorted = 
+        System.Linq.Enumerable.OrderBy(xs, fun x -> x) 
+        |> List.ofSeq
+    quickSorted = systemSorted
+
+Check.Quick quickSortIsIdempotent 
+Check.Quick quickSortMatchesEnumerableOrderBy 
+
+let replayConfig = 
+    { Config.Verbose with 
+        Config.Replay = Some <| (Random.StdGen (1499822617,295966976)) }
+Check.One (replayConfig, quickSortMatchesEnumerableOrderBy)
