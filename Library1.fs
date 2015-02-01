28be32594dd40b0db4513542fb949172a576529b
Andrew Browne <brownie@brownie.com.au>
2015-02-01 22:59:58 +1100

    Added Gen and Classify examples

--- Library1.fs
+++ Library1.fs
@@ -1,7 +1,32 @@
 ﻿namespace FsCheckIntro
 
+open FsCheck
 open FsCheck.Xunit
 
+type NegativeNumber = NegativeNumber of int
+
+type ListOfPositiveIntegers = ListOfPositiveIntegers of int list
+
+type MyArbitraryInstances =
+    static member NegativeNumber() =
+        Arb.from<int>
+        |> Arb.filter (fun x -> x < 0)
+        |> Arb.convert (NegativeNumber) (fun (NegativeNumber i) -> i)
+
+    static member ListOfPositiveIntegers() =
+        let rec nextValue = function
+        | 0 -> gen { return [] }
+        | x -> gen {
+            let! x = Gen.suchThat (fun x -> x > 0) Arb.generate 
+            let! rest = (nextValue <| x - 1)
+            return x :: rest
+        }
+        
+        Gen.sized nextValue
+        |> Arb.fromGen
+        
+
+[<Arbitrary(typeof<MyArbitraryInstances>)>]
 module Tests = 
 
     let rec quickSort = function
@@ -23,3 +48,34 @@ module Tests =
             System.Linq.Enumerable.OrderBy(xs, fun x -> x) 
             |> List.ofSeq
         quickSorted = systemSorted
+
+    let abs_x_not_x x = 
+        printfn "value: %d" x
+        abs x <> x
+
+    [<Property(Verbose = true)>]
+    let ``abs of a negative number is not equal to itself 1`` (x : int) =
+        (x < 0) ==> abs_x_not_x x
+
+    [<Property>]
+    let ``abs of a negative number is not equal to itself 2`` () =
+        let negativeNumber =
+            Arb.from<int>
+            |> Arb.filter (fun x -> x < 0)
+
+        Prop.forAll negativeNumber abs_x_not_x
+
+    [<Property>]
+    let ``abs of a negative number is not equal to itself 3`` (NegativeNumber x) =
+       abs_x_not_x x
+
+    [<Property>]
+    let ``all positive`` (ListOfPositiveIntegers xs) =
+        xs |> List.forall (fun x -> x > 0)
+
+    [<Property>]
+    let ``all positive categorized`` (ListOfPositiveIntegers xs) =
+        xs |> List.forall (fun x -> x > 0)
+        |> Prop.classify (xs = []) "Empty list"
+        |> Prop.classify (xs <> []) "Non Empty list"
+        |> Prop.classify (xs |> List.length > 10) "More than 10 elements"
